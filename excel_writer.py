0fd91baa1a7494c553150a69322e626c9408b414
Laurie Green <lauriegreen18@gmail.com>
2015-05-21 23:56:48 +0100

    Excel writer can now read dates

--- excel_writer.py
+++ excel_writer.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.cell import coordinate_from_string, column_index_from_string
 
-wb = load_workbook('I:\Storage\Documents\Dev Projects\Excel\\test.xlsx')
+wb = load_workbook('I:\Storage\Documents\Dev Projects\Excel\\test.xlsx', data_only=True)
 
 # grab the active worksheet
 ws = wb.active
@@ -20,8 +21,12 @@ for colomn in ws.columns:
 
 for colomn in ws.columns:
 	for cell in colomn:
-		if cell.value == target_date:
-			cell.offset(target_row-3,0).value = holiday
+		if cell.is_date == True:
+			x = str(cell.value)
+			string_date = datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime('%d/%m/%Y')
+			if string_date == target_date:
+				print "found it!"
+				cell.offset(target_row-3,0).value = holiday
 
 # Save the file
 wb.save('I:\Storage\Documents\Dev Projects\Excel\\test.xlsx')
